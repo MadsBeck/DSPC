f4ebf2712175b978e35d56adb97427c23683699c
Mads Mørk Beck <au538009@uni.au.dk>
2018-06-14 11:53:43 +0200

    Stagin 490 files, guess what i did?!
    Yes, i added the nios2 and and RS232 to the project, THX INTEL!

---
+++ HashComp/software/Rs232Comm/obj/default/hello_world.d
@@ -0,0 +1,17 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Rs232Comm_bsp//HAL/inc/alt_types.h ../Rs232Comm_bsp/system.h \
+ ../Rs232Comm_bsp/linker.h ../Rs232Comm_bsp//HAL/inc/io.h \
+ ../Rs232Comm_bsp//HAL/inc/alt_types.h \
+ ../Rs232Comm_bsp//HAL/inc/sys/alt_log_printf.h
+
+../Rs232Comm_bsp//HAL/inc/alt_types.h:
+
+../Rs232Comm_bsp/system.h:
+
+../Rs232Comm_bsp/linker.h:
+
+../Rs232Comm_bsp//HAL/inc/io.h:
+
+../Rs232Comm_bsp//HAL/inc/alt_types.h:
+
+../Rs232Comm_bsp//HAL/inc/sys/alt_log_printf.h:
